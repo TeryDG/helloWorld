3a278be1e1225060e2ad3f07939416fad232c17e
TeryDG <137882368+TeryDG@users.noreply.github.com>
2024-01-15 15:11:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# app.py
+import streamlit as st
+
+def main():
+    st.title("Hello, World!")
+    st.write("This is a simple Streamlit app deployed on GitHub.")
+
+if __name__ == "__main__":
+    main()
