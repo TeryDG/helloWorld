ed3da0dc04b627b25abae5179aee45605148baec
TeryDG <137882368+TeryDG@users.noreply.github.com>
2024-01-15 15:18:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # app.py
 import streamlit as st
 
-def main():
     st.title("Hello, World!")
     st.write("This is a simple Streamlit app deployed on GitHub.")
 
-if __name__ == "__main__":
-    main()
+
